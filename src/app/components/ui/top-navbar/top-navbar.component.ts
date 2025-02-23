1585c2ab66bac997a4c88392757617a4abb8aa84
buckapi <buckapis@gmail.com>
2025-02-23 16:44:19 -0500

    ---

--- src/app/components/ui/top-navbar/top-navbar.component.ts
+++ src/app/components/ui/top-navbar/top-navbar.component.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { Component, HostListener } from '@angular/core';
 import { GlobalService } from '@app/services/global.service';
 import { virtualRouter } from '@app/services/virtualRouter.service';
@@ -5,13 +6,15 @@ import { virtualRouter } from '@app/services/virtualRouter.service';
 @Component({
   selector: 'app-top-navbar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './top-navbar.component.html',
   styleUrl: './top-navbar.component.css'
 })
 export class TopNavbarComponent {
   phoneNumber: string = '9198855401';
   menuOpen: boolean = false;
+  isFabMenuOpen: boolean = false; // Cambia el nombre aquí
+
   constructor(
     public global:GlobalService,
     public virtualRouter: virtualRouter
@@ -21,6 +24,10 @@ export class TopNavbarComponent {
     event.stopPropagation();
   }
 
+  toggleFabMenu(event: MouseEvent) {
+    this.isFabMenuOpen = !this.isFabMenuOpen; // Alterna el estado del menú
+    event.stopPropagation(); // Evita que el evento se propague
+  }
 
   sendSMS() {
     window.open(`sms:${this.phoneNumber}`, '_self');
